fd26ac2ce748f7f573b9179adf154a1cd5970b1d
Martynas Mickevičius <self@2m.lt>
2023-11-27 21:17:56 +0200

    Use egui native icon support

--- build.rs
+++ build.rs
@@ -1,15 +1,3 @@
-#[cfg(windows)]
-extern crate winres;
-
-#[cfg(windows)]
-fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("icon.ico");
-    res.compile().unwrap();
-    built::write_built_file().expect("Failed to acquire build-time information");
-}
-
-#[cfg(unix)]
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
 }
